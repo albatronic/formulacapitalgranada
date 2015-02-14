7343c99d84b099a04f698a444d1a03e587b578a7
Sergio Pérez <sergio.perez@albatronic.com>
2015-02-14 21:42:11 +0100

    contacto

--- themes/themeDefault/modules/Contacto/ContactoController.class.php
+++ themes/themeDefault/modules/Contacto/ContactoController.class.php
@@ -18,7 +18,14 @@ class ContactoController extends ControllerProject {
         switch ($this->request['METHOD']) {
             case 'GET':
                 $this->values['accion'] = "";
-                mail("info@albatronic.com","Credit Granada", "Han entrado en el formulario de contacto general");
+                if ($this->request[1] == "log") {
+                    return $this->Log();
+                } else {
+                    mail($this->varWeb['Pro']['mail']['from'], "Credit Granada", "Han entrado en el formulario de contacto general");
+                    $fp = fopen("log/contacto.log", "a");
+                    fwrite($fp, date("Y-m-d H:i:s") . ";" . $_SERVER['REMOTE_ADDR'] . ";Entran en contacto principal" . "\n");
+                    fclose($fp);
+                }
                 break;
 
             case 'POST':
@@ -62,7 +69,7 @@ class ContactoController extends ControllerProject {
         $plantilla = file_get_contents($ficheroPlantilla);
         $plantilla = str_replace("#MENSAJE#", $this->varWeb['Pro']['mail']['mensajeConfirmacion'], $plantilla);
         $plantilla = str_replace("#TEXTOLOPD#", $this->varWeb['Pro']['mail']['textoLOPD'], $plantilla);
-        
+
         return $mailer->send(
                         $this->request['email'], $this->varWeb['Pro']['mail']['from'], $this->varWeb['Pro']['mail']['from_name'], 'Hemos recibido su mensaje', $plantilla, array()
         );
@@ -79,8 +86,8 @@ class ContactoController extends ControllerProject {
     private function enviaWebMaster($mailer, $ficheroPlantilla) {
 
         $plantilla = file_get_contents($ficheroPlantilla);
-        
-        $mensaje  = "<p style='margin-bottom: 20px;'><strong>Han dejado el siguiente mensaje en el formulario de contacto:</strong></p>";
+
+        $mensaje = "<p style='margin-bottom: 20px;'><strong>Han dejado el siguiente mensaje en el formulario de contacto:</strong></p>";
         $mensaje .= "<p>Nombre: {$this->request['nombre']}</p>";
         $mensaje .= "<p>Apellidos: {$this->request['apellidos']}</p>";
         $mensaje .= "<p>Email: {$this->request['email']}</p>";
@@ -91,12 +98,32 @@ class ContactoController extends ControllerProject {
         $mensaje .= "<p>Solucion: {$this->request['solucion']}</p>";
         $mensaje .= "<p>Importe: {$this->request['importe']}</p>";
         $mensaje .= "<p>Observaciones: {$this->request['observaciones']}</p>";
-        
+
         $plantilla = str_replace("#TEXTOLOPD#", $this->varWeb['Pro']['mail']['textoLOPD'], $plantilla);
-        $plantilla = str_replace("#MENSAJE#", $mensaje, $plantilla);        
+        $plantilla = str_replace("#MENSAJE#", $mensaje, $plantilla);
 
         return $mailer->send(
                         $this->varWeb['Pro']['mail']['from'], $this->request['email'], $this->request['nombre'], 'Ha recibido un mensaje en la web', $plantilla, array()
+        );
+    }
+
+    public function Log() {
+
+        $log = "log/contacto.log";
+        if (file_exists($log)) {
+            $fp = fopen($log, "r");
+            $this->values['csv'] = fread($fp, filesize($log));
+            fclose($fp);
+        }
+
+        header("Content-type: text/csv");
+        header("Content-Disposition: attachment; filename=contacto.csv");
+        header("Pragma: no-cache");
+        header("Expires: 0");
+
+        return array(
+            'template' => $this->entity . "/log.csv.twig",
+            'values' => $this->values,
         );
     }
 
