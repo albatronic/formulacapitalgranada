8e5028084b064337bcf3f40317a088b9a36b1f9d
Sergio Pérez <sergio.perez@albatronic.com>
2015-03-08 20:27:57 +0100

    inmobiliaria

---
+++ themes/themeDefault/modules/Inmobiliaria/InmobiliariaController.class.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Description of ContenidoController
+ *
+ * @author Sergio Pérez <sergio.perez@albatronic.com>
+ * @copyright Informática ALBATRONIC, SL
+ * @date 06-agosto-2014
+ */
+class InmobiliariaController extends ControllerProject {
+
+    var $entity = "Inmobiliaria";
+
+    public function IndexAction() {
+
+        $this->values['menuLateral'] = Menu::getMenuN(2);
+        $this->values['contenido'] = new GconSecciones($this->request['IdEntity']);
+
+        $template = ($this->request['Template'] !== '') ? $this->entity . "/" . $this->request['Template'] :
+                $this->controller . "/index.html.twig";
+        
+        return array(
+            'template' => $template,
+            'values' => $this->values,
+        );
+    }
+
+}
