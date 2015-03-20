264c1dc5b7d999b9c3ff677d97dee5c273c163e4
Sergio Pérez <cgcg@serv1.servidordns.com.es>
2015-03-20 01:12:27 +0100

    filtro inmuebles

--- entities/methods/Inmuebles.class.php
+++ entities/methods/Inmuebles.class.php
@@ -10,8 +10,48 @@
  */
 class Inmuebles extends InmueblesEntity {
 
+     var $_tiposOperacion = array(
+        'V' => 'Venta',
+    );
+    
     public function __toString() {
         return ($this->Id) ? $this->Id : '';
+    }
+
+    public function getProvinciasPoblacionesZonas() {
+
+        $array = array();
+
+        $rows = $this->cargaCondicion("des_provincia provincia, des_poblacion poblacion, des_zona zona", "", "des_provincia ASC, des_poblacion ASC, des_zona ASC");
+        foreach ($rows as $row) {
+            $array[$row['provincia']][$row['poblacion']][$row['zona']] = $row['zona'];
+        }
+
+        return $array;
+    }
+    
+    public function getTiposOperacion(){
+ 
+        $array = array();
+
+        $rows = $this->cargaCondicion("distinct(cod_destino) destino", "");
+        foreach ($rows as $row) {
+            $array[$row['destino']] = $this->_tiposOperacion[$row['destino']];
+        }
+
+        return $array;       
+    }
+    
+    public function getTiposInmueble(){
+ 
+        $array = array();
+
+        $rows = $this->cargaCondicion("distinct(des_tipoelem) tipo", "", "des_tipoelem ASC");
+        foreach ($rows as $row) {
+            $array[$row['tipo']] = $row['tipo'];
+        }
+
+        return $array;       
     }
 
 }
