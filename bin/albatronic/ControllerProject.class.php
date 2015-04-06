ad2ca9b15e7ebb704d822764224f348d54765136
Sergio Pérez <sergio.perez@albatronic.com>
2015-04-06 21:08:23 +0200

    menu superior

--- bin/albatronic/ControllerProject.class.php
+++ bin/albatronic/ControllerProject.class.php
@@ -20,7 +20,7 @@ class ControllerProject extends ControllerWeb {
         $this->values['ruta'] = $this->getRuta();
         //$this->values['calendario'] = Calendario::getCalendario();
         $this->values['ustedEstaEn'] = $this->getUstedEstaEn();
-        $this->values['menuCabecera'] = Menu::getMenuDesplegable(1,6);
+        $this->values['menuCabecera'] = Menu::getMenuDesplegable(1,7);
         $this->values['menuPie'] = Menu::getMenuN(3,8);    
         $this->values['serviciosPie'] = Servicios::getServicios(0, true, 7);
         $this->values['familiasServicios'] = Servicios::getFamilias();
